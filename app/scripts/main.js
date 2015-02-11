57278a2d6613b68c4e6667348e384067466d390e
adamish717 <adam@piechnik.ca>
2015-02-11 13:40:10 -0800

    Add content for frameworking

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var $root = $('html, body');
+$('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
